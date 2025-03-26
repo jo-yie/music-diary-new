38f889bb23080b7f1f7e9e21f27c1fb0f206ee22
Jo Yie <joyieleong@gmail.com>
2025-03-27 05:03:55 +0800

    removed service worker

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -42,13 +42,13 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     GoogleMapsModule,
     FormsModule,
     MaterialModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      // enabled: true,
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    // ServiceWorkerModule.register('ngsw-worker.js', {
+    //   // enabled: true,
+    //   enabled: !isDevMode(),
+    //   // Register the ServiceWorker as soon as the application is stable
+    //   // or after 30 seconds (whichever comes first).
+    //   registrationStrategy: 'registerWhenStable:30000'
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
